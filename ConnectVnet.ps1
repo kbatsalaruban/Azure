ef8c722fe8e75a6910ce71d4f66233e2a08ab486
kbatsalaruban <kishen.batsalaruban@gmail.com>
2016-07-29 11:23:48 +1000

    Connect two VNets in Azure
    useful for Vnet to Vnet connections using a preshared key

---
+++ ConnectVnet.ps1
@@ -0,0 +1,11 @@
+﻿Get-AzureVnetConfig -ExporttoFile "path\filename.xml"
+#This XML can then be edited and uploaded to the Azure fabric for execution - friggin awesome
+
+Set-AzureVNetConfig -ConfigurationPath "path\filename.xml"
+#Finally, set the IPSec shared key
+
+PS D:\> Set-AzureVNetGatewayKey -VNetName VNet01 -LocalNetworkSiteName VNet02 -SharedKey Keldai881244
+PS D:\> Set-AzureVNetGatewayKey -VNetName VNet02 -LocalNetworkSiteName VNet01 -SharedKey Keldai881244
+
+#Check connectivity
+PS D:\> Get-AzureVNetConnection -VNetName VNet1 | ft LocalNetworkSiteName, ConnectivityState
